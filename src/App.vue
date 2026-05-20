d077e3b26613519cc9993ff231508da69fe653d8
José Xavier <josexavier46@outlook.pt>
2026-05-20 23:38:54 +0100

    initial project commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <h1>Hello World!</h1>
+    <p>CI/CD project developed by José Xavier.</p>
+  </main>
+</template>
+
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  font-family: sans-serif;
+  background-color: #f4f4f9;
+  color: #333;
+}
+
+h1 {
+  color: #42b883;
+}
+</style>
